9cd1982db1232cc082045910793b545e8e3722ee
JHON AEDRIAN DUMOL (DEV-DIGITAL-ISD-OOCLL/MNL) <dumoljh@oocl.com>
2022-08-19 12:50:46 +0800

    Added answer for multiplication table and number

---
+++ multiplication_table/index.js
@@ -0,0 +1,41 @@
+// ## requirement
+
+// Create new index.js file, write program output 9*9 times table. The output is as follows：
+
+// ```
+// 1*1=1
+// 2*1=2 2*2=4
+// 3*1=3 3*2=6 3*3=9
+// ……
+// 9*1=9 9*2=18 9*3=27 …… 9*9=81
+// ```
+
+var a = [1,2,3,4,5,6,7,8,9]
+
+for (let i = 1; i <= 1; i++) {
+	console.log(a[0] + "*" + i + "=" + a[0] * i);
+	  }
+for (let i = 1; i <= 2; i++) {
+	console.log(a[1] + "*" + i + "=" + a[1] * i);
+	  }
+for (let i = 1; i <= 3; i++) {
+	console.log(a[2] + "*" + i + "=" + a[2] * i);
+	  }
+for (let i = 1; i <= 4; i++) {
+	console.log(a[3] + "*" + i + "=" + a[3] * i);
+	  }
+for (let i = 1; i <= 5; i++) {
+	console.log(a[4] + "*" + i + "=" + a[4] * i);
+	  }
+for (let i = 1; i <= 6; i++) {
+	console.log(a[5] + "*" + i + "=" + a[5] * i);
+	  }
+for (let i = 1; i <= 7; i++) {
+	console.log(a[6] + "*" + i + "=" + a[6] * i);
+	  }
+for (let i = 1; i <= 8; i++) {
+	console.log(a[7] + "*" + i + "=" + a[7] * i);
+	  }
+	  for (let i = 1; i <= 9; i++) {
+	console.log(a[8] + "*" + i + "=" + a[8] * i);
+	  }
